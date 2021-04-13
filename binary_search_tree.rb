bccebd38de535e0105079d390cbbd461109977b9
Guillermo <guillermoc93@protonmail.com>
2021-04-13 10:43:47 -0500

    working insert method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -27,11 +27,22 @@ class Tree
     root
   end
 
+  def insert(value, root = @root)
+    return Node.new(value) if root == nil
+    return root if root.data == value
+
+    if value < root.data
+      root.left = insert(value, root.left)
+    else
+      root.right = insert(value, root.right)
+    end
+
+    root
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
-
-p binary_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
