518072a58e19550292565d5d2f5280956cfffb95
Guillermo <guillermoc93@protonmail.com>
2021-04-13 09:43:05 -0500

    working build tree method, added pretty print method to visualize tree.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -11,8 +11,27 @@ end
 class Tree
   attr_accessor :arr, :root
 
-  def initiliaze(arr)
+  def initialize(arr)
     @arr = arr
-    @root = build_tree
+    @root = build_tree(@arr)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    sorted = array.uniq.sort
+    mid = sorted.length / 2
+    root = Node.new(sorted[mid])
+    root.left = build_tree(sorted[0...mid])
+    root.right = build_tree(sorted[mid + 1..sorted.length])
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+p binary_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
